53faac35927498488e937e046f24e4bd86fb7f3f
Romulo <romulosousa@fabricainfo.com>
2020-03-25 15:24:43 -0300

    Rotas : Profile , Ongs, Incidents

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,9 +1,25 @@
 const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.send('Hello Node');
-});
+/** Session */
+routes.post('/sessions',SessionController.create);
 
+/** Ongs */
+routes.get('/ongs',OngController.index); 
+routes.post('/ongs', OngController.store);
+
+/** Profile */
+routes.get('/profile', ProfileController.index);
+
+/** Incidents */
+routes.get('/incidents',IncidentController.index); 
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
