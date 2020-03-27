9dd03051b93d710502cfcfcb167c6a1384325bf7
Romulo <romulosousa@fabricainfo.com>
2020-03-27 18:58:19 -0300

    Sessão de Login do usuário.

--- front-end/src/pages/Logon/index.js
+++ front-end/src/pages/Logon/index.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import {FiLogIn} from 'react-icons/fi';
 
 import './styles.css';
+import api from "../../services/api";
+
 import heroes from '../../assets/heroes.png';
 import logo from '../../assets/logo.svg';
 
-function Logon() {
+export default function Logon() {
+    const [id, setId] = useState('');
+
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('/sessions', {id});
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName',response.data.name);
+
+            history.push('/profile');
+        }catch (err) {
+            alert('Erro ao efetuar o Login');
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logo} alt="Be The Hero"/>
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu Logon</h1>
-                    <input placeholder="Sua ID " type="text"/>
+                    <input placeholder="Insira o id da ONG"
+                           value={id}
+                           onChange={e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register"><FiLogIn size={16} color="#e02041"/> Não tenho cadastro</a>
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#e02041"/> Não tenho cadastro
+                    </Link>
                 </form>
 
             </section>
@@ -25,4 +51,4 @@ function Logon() {
     );
 }
 
-export default Logon;
+
